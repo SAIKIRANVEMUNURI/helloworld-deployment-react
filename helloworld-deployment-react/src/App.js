a7e162742fafeb7c4422b5fe2415cf478dd6ed9b
saikiranvemunuri <saikiranreddyvemunuri@gmail.com>
2024-12-26 22:35:37 -0800

    created react app

---
+++ helloworld-deployment-react/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple React Application</h1>
+        <p>
+          Creating HelloWorld UI App to build and deploy through AWS
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
